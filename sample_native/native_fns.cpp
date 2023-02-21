c5add0d2f95f1992488d32d639dc3aca8512ba50
Arjun Ramesh <arjunr2@andrew.cmu.edu>
2023-02-21 14:41:25 -0500

    Test timing for pure compute

--- sample_native/native_fns.cpp
+++ sample_native/native_fns.cpp
@@ -1,65 +1,65 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <stdlib.h>
+#include <sys/mman.h>
 #include "wasm_export.h"
 
 #include <map>
 #include <set>
 #include <mutex>
 
-std::mutex mtx;
+/* Timing */
+uint64_t start_ts;
+uint64_t end_ts;
+
+static inline uint64_t ts2us(struct timespec ts) {
+  return (ts.tv_sec * 1000000) + (ts.tv_nsec / 1000);
+}
+
+uint64_t gettime() {
+  struct timespec ts;
+  clock_gettime(CLOCK_REALTIME, &ts);
+  return ts2us(ts);
+}
+/* */
+
+
+/* Access Logger */
 struct acc_entry {
   wasm_exec_env_t last_tid;
   uint64_t freq;
   bool shared;
-  std::set<uint32_t> shared_inst_idxs;
-  acc_entry() : freq(0), shared(0), last_tid(NULL) { };
+  acc_entry() : last_tid(NULL), freq(0), shared(0) { };
 };
 
-/* Log data */
-std::map<uint32_t, acc_entry> access_table;
+std::mutex mtx;
+acc_entry *access_table = NULL;
+std::set<uint32_t> shared_inst_idxs;
+/*  */
+
+
 
 void logaccess_wrapper(wasm_exec_env_t exec_env, uint32_t addr, uint32_t opcode, uint32_t inst_idx) {
   mtx.lock();
-  acc_entry &entry = access_table[addr];
-  bool new_tid_acc = exec_env != entry.last_tid;
+  acc_entry *entry = access_table + addr;
+  bool new_tid_acc = exec_env != entry->last_tid;
   /* Flag as shared memory if different tids access it */
-  if (entry.last_tid && new_tid_acc) {
-    entry.shared = true;
+  if (entry->last_tid && new_tid_acc) {
+    entry->shared = true;
   }
   /* Log indexes to conflicting address */
-  if (entry.shared || new_tid_acc) {
-    entry.shared_inst_idxs.insert(inst_idx);
+  if (entry->shared || new_tid_acc) {
+    shared_inst_idxs.insert(inst_idx);
   }
-  entry.last_tid = exec_env;
-  entry.freq += 1;
+  entry->last_tid = exec_env;
+  entry->freq += 1;
   mtx.unlock();
 }
 
 void logend_wrapper(wasm_exec_env_t exec_env) {
-  //printf("=== ACCESS TABLE ===\n");
-  //for (auto const& x : access_table) {
-  //  const acc_entry &entry = x.second;
-  //  const std::set<uint32_t> &shared_idxs = entry.shared_inst_idxs;
-  //  if (x.second.shared) {
-  //    printf("Addr [%u] | Accesses: %lu  [SHARED by %lu insts: ", 
-  //      x.first, x.second.freq, shared_idxs.size());
-  //    for (auto &v : shared_idxs) {
-  //      printf("%u, ", v);
-  //    }
-  //    printf("]\n");
-  //  } else {
-  //    printf("Addr [%u] | Accesses: %lu (ISOLATED: %u)\n", 
-  //      x.first, x.second.freq, *(shared_idxs.begin()));
-  //  }
-  //}
-  //printf("================\n");
-
-  //printf("=== Instruction Indexes ===\n");
-  //printf("Num instructions: %u\n", shared_inst_idxs.size());
-  //for (auto &idx : shared_inst_idxs) {
-  //  printf("%d\n", idx);
-  //}
-  //printf("================\n");
+  end_ts = gettime();
+  float total_time = (float)(end_ts - start_ts) / 1000000; 
+  printf("Time taken: %.3f\n", total_time);
 }
 
 #define REG_NATIVE_FUNC(func_name, sig) \
@@ -69,8 +69,18 @@ static NativeSymbol native_symbols[] = {
   REG_NATIVE_FUNC(logend, "()")
 };
 
+void init_acc_table() {
+  size_t size = ((size_t)1 << 32);
+  access_table = (acc_entry*) malloc(sizeof(acc_entry) * size);
+  if (access_table == NULL) {
+    perror("malloc error");
+  }
+  start_ts = gettime();
+}
+
 extern "C" uint32_t get_native_lib (char **p_module_name, NativeSymbol **p_native_symbols) {
   *p_module_name = "instrument";
+  init_acc_table();
   *p_native_symbols = native_symbols;
   return sizeof(native_symbols) / sizeof(NativeSymbol);
 }
