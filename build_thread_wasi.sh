045af0469ec8014804eecea4e949aaf65807e922
Arjun Ramesh <arjunr2@andrew.cmu.edu>
2023-02-23 17:58:17 -0500

    Added wasi build support + instrumentation update

---
+++ build_thread_wasi.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+shopt -s expand_aliases
+source ~/.alias
+
+WAMR_ROOT=/home/arjun/Documents/research/arena/wamr-conix
+WAMRC=$WAMR_ROOT/wamr-compiler/build/wamrc
+
+name=$(basename $1 .c)
+
+/opt/wasi-sdk-16/bin/clang --target=wasm32-wasi \
+    -O3 -pthread       \
+    -Wl,--shared-memory             \
+    -Wl,--allow-undefined,--no-check-features \
+    -Wl,--export=__heap_base,--export=__data_end    \
+    $name.c -o $name.wasm
+# -pthread: it will enable some dependent WebAssembly features for thread
+# -nostdlib: disable the WASI standard library as we are using WAMR builtin-libc
+# -z stack-size=: specify the total aux stack size
+# -Wl,--export=__heap_base,--export=__data_end: export these globals so the runtime can resolve the total aux stack size and the start offset of the stack top
+# -Wl,--export=__wasm_call_ctors: export the init function to initialize the passive data segments
+
+wasm2wat --enable-threads $name.wasm -o $name.wat
+$WAMRC --enable-multi-thread -o $name.aot $name.wasm
+$WAMRC --target=aarch64 --enable-multi-thread -o $name-arm64.aot $name.wasm
+
+./instrument -o out.wasm $name.wasm
+$WAMRC --enable-multi-thread -o out.aot out.wasm
+$WAMRC --target=aarch64 --enable-multi-thread -o out-arm64.aot out.wasm
+
