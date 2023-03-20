4307951873cb55a7e34a20ed0d2a4859ff9f23ea
Arjun Ramesh <arjunr2@andrew.cmu.edu>
2023-03-20 00:38:15 -0400

    Added benchmark script update

--- benchmark.sh
+++ benchmark.sh
@@ -3,16 +3,20 @@
 RES_DIR=results
 mkdir -p $RES_DIR
 
-# Run all instructions instrumented access
-make access
-./run_access.py all &> $RES_DIR/single_run.res
+for run_no in {1..20}; do
+  for batch_size in {2..8..2}; do
+    make
+    make access
+    make -B access-batch BATCH_SIZE=$batch_size
 
-for run_no in {1..10}; do
-  for batch_size in {2..10..2}; do
-    for id in {10..90..10}; do
-      make -B access-batch STOCH=$id BATCH_SIZE=$batch_size
-      ./run_access.py --batch=$batch_size all &> $RES_DIR/run_${run_no}_${id}_${batch_size}.res
-    done
+    sleep 4
+
+    # Run no instrumentation 
+    ./run_tests.py --mode=normal all &> $RES_DIR/noinst_run_${run_no}_${batch_size}.res
+    # Run all instructions instrumented access
+    ./run_tests.py --mode=access all &> $RES_DIR/allinst_run_${run_no}_${batch_size}.res
+    # Run sharded instrumentation access
+    ./run_tests.py --mode=access --batch=$batch_size all &> $RES_DIR/run_${run_no}_${batch_size}.res
   done
 done
 
